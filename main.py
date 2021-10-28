7df3623af6ad7aa2d120f32467bc844edcd04db8
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-10-28 12:46:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def start(client: Pr0fess0r_99, update):
             ]
         ] 
     )                       
-    message.reply_photo(
+    bot.reply_photo(
     photo=f"{random.choice(PHOTO)}",
     caption="👋Hy {}, Iam Moscow\n\nI cant work in your group",
     parse_mode="html"

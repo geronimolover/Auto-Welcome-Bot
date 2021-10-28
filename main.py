da95d594e9424a205047741cb6f86965d526ce0b
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-10-28 12:25:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ async def start(client: Pr0fess0r_99, update):
             ]
         ] 
     )                       
-    client.send_photo(
-    chat_id=message.chat.id,
+    client.reply_photo(
     photo=f"{random.choice(PHOTO)}",
     caption="👋Hy {}, Iam Moscow\n\nI cant work in your group",
     parse_mode="html"

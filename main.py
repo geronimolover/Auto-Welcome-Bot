6fd76a15f61d03692cf519441658b917c9b4aeb2
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-12-03 12:06:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ PHOTO = [
 
 @Pr0fess0r_99.on_message(filters.command("start"))
 async def start(client: Pr0fess0r_99, update):
-    start_msg = "👋Hy {}, Iam Simple Auto Welcome Bot"
+    start_msg = "👋Hai {msg.from_user.mention} , Iam Simple Auto Welcome Bot"
     bot_username = await client.get_me()
     link = "PR0FESS0R-99/Auto-Welcome-Bot"
     reply_markup = InlineKeyboardMarkup(

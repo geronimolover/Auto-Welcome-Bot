4e6562da1ceaa35e1ceb7e1f10d9821e6ab0591f
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-12-03 13:07:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -116,8 +116,11 @@ async def auto_welcome(bot: Pr0fess0r_99, msg: Message):
     button_name = os.environ.get("WELCOME_BUTTON_NAME", name_button)
     button_link = os.environ.get("WELCOME_BUTTON_LINK", link_button)
     welcome_text = f"Hey {mention}\nWelcome To {group_name}"
-    WELCOME_TEXT = os.environ.get("WELCOME_TEXT", welcome_text)
-    print("Welcome Message Activate")
+    WELCOME_TEXT = """Hi {first},
+
+Welcome to AK Movie Group
+
+Just send movie name. Please don't send anything with movie name. Please read the group rules. This group is only meant for movie requesting please don't send anything other"""
     BUTTON = bool(os.environ.get("WELCOME_BUTTON"))
     if not BUTTON:
        k = await msg.reply_text(text=WELCOME_TEXT.format(
